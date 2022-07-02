37d4dce6871e99111a5c0bd7f14ea5bffaf4c08c
Rune Madsen <rune@runemadsen.com>
2022-07-02 16:01:51 +0200

    update and delete working

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -1,30 +1,23 @@
 const { proxy } = require("@designsystemsinternational/react-admin-github/api");
 
 const handler = async (event) => {
-  try {
-    const response = await proxy({
-      repo: "designsystemsinternational/react-admin-github-example",
-      httpMethod: event.httpMethod,
-      query: event.queryStringParameters,
-      headers: event.headers,
-      body: event.body,
-      token: process.env.GITHUB_TOKEN,
-      secret: process.env.SECRET,
-      resourceIds: {
-        posts: "title",
-      },
-    });
-    return {
-      statusCode: 200,
-      body: JSON.stringify(response),
-    };
-  } catch (e) {
-    console.log("ERROR", e);
-    return {
-      statusCode: e.status,
-      body: JSON.stringify({ error: e.message }),
-    };
-  }
+  const { statusCode, body } = await proxy({
+    repo: "designsystemsinternational/react-admin-github-example",
+    httpMethod: event.httpMethod,
+    query: event.queryStringParameters,
+    headers: event.headers,
+    body: event.body,
+    token: process.env.GITHUB_TOKEN,
+    secret: process.env.SECRET,
+    resourceIds: {
+      posts: "title",
+    },
+  });
+  console.log("proxy", statusCode, body);
+  return {
+    statusCode,
+    body: JSON.stringify(body),
+  };
 };
 
 module.exports = {
